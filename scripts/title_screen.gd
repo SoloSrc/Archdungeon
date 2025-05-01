a6d9d30ebf938b2c1c3b99575678db952920b5a4
Vítor de Albuquerque Torreão <vitorreao@pm.me>
2025-04-30 23:15:36 -0300

    Implement Tutorial button on title screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+const BUSY_HOURGLASS_OUTLINE_DETAIL = preload("res://assets/kenney_cursor-pack/Vector/Outline/busy_hourglass_outline_detail.svg")
+const POINTER_C = preload("res://assets/kenney_cursor-pack/Vector/Outline/pointer_c.svg")
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,3 +17,9 @@ func _process(delta: float) -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_tutorial_pressed() -> void:
+	Input.set_custom_mouse_cursor(BUSY_HOURGLASS_OUTLINE_DETAIL)
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
+	Input.set_custom_mouse_cursor(POINTER_C)
